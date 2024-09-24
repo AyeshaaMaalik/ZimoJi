da62862d479e11ce0d9c4da51b060aebe580922d
ash <113773607+Aishaamalik@users.noreply.github.com>
2024-09-24 10:28:43 +0500

    Adding Custom drawer

--- Screens/Drawer/DrawerNavigator.jsx
+++ Screens/Drawer/DrawerNavigator.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { View, Image, StyleSheet, Text } from 'react-native';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import Scan from './Scan';
 import ScanImage from './ScanImage';
 import History from './History';
@@ -9,17 +10,163 @@ import ChooseLogo from './ChooseLogo';
 
 const Drawer = createDrawerNavigator();
 
-const DrawerNavigator = () => {
+const CustomDrawerContent = (props) => {
   return (
-      <Drawer.Navigator initialRouteName="Scan">
-        <Drawer.Screen name="Scan" component={Scan} />
-        <Drawer.Screen name="Scan Image" component={ScanImage} />
-        <Drawer.Screen name="History" component={History} />
-        <Drawer.Screen name="Favorites" component={Favorites} />
-        <Drawer.Screen name="Contacts" component={Contacts} />
-        <Drawer.Screen name="Choose Application Logo" component={ChooseLogo} />
-      </Drawer.Navigator>
+    <DrawerContentScrollView {...props} style={styles.drawer}>
+      <View style={styles.logoContainer}>
+        <Image 
+          source={require('../Assets/SplashWhite.png')} 
+          style={styles.logo}
+        />
+      </View>
+      <DrawerItemList {...props} />
+      <View style={styles.footerSpacer} />
+      <View style={styles.footerContainer}>
+        <Text style={styles.footerText}>
+          THIS APPLICATION IS DESIGNED TO BE USED
+        </Text>
+        <Text style={styles.footerText1}>
+        EXCLUSIVELY WITH ZIMOJI'S.
+        </Text>
+        <Text style={styles.footerText}>
+          COPYRIGHT © 2024 ZIMOJI | ZIMO GROUP LIMITED.
+        </Text>
+        <Text style={styles.footerText1}>
+          ALL RIGHTS RESERVED.
+        </Text>
+        <Text style={styles.footerText}>
+          TERMS | PRIVACY
+        </Text>
+      </View>
+    </DrawerContentScrollView>
   );
 };
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Scan"
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerStyle: { backgroundColor: '#000' }, 
+        drawerLabelStyle: { color: '#fff' },
+      }}
+    >
+      <Drawer.Screen 
+        name="SCAN" 
+        component={Scan} 
+        options={{ 
+          drawerIcon: ({ focused, size }) => (
+            <Image
+              source={require('../Assets/Scan.png')}
+              style={[styles.icon, { width: size * 1.5, height: size * 1.5 }]}
+            />
+          ),
+        }} 
+      />
+      <Drawer.Screen 
+        name="SCAN IMAGE" 
+        component={ScanImage} 
+        options={{ 
+          drawerIcon: ({ focused, size }) => (
+            <Image
+              source={require('../Assets/ScanImage.png')} 
+              style={[styles.icon, { width: size * 1.5, height: size * 1.5 }]} 
+            />
+          ),
+        }} 
+      />
+      <Drawer.Screen 
+        name="HISTORY" 
+        component={History} 
+        options={{ 
+          drawerIcon: ({ focused, size }) => (
+            <Image
+              source={require('../Assets/History.png')} 
+              style={[styles.icon, { width: size * 1.5, height: size * 1.5 }]} 
+            />
+          ),
+        }} 
+      />
+      <Drawer.Screen 
+        name="FAVOURITES" 
+        component={Favorites} 
+        options={{ 
+          drawerIcon: ({ focused, size }) => (
+            <Image
+              source={require('../Assets/Favourites.png')}
+              style={[styles.icon, { width: size * 1.5, height: size * 1.5 }] }
+            />
+          ),
+        }} 
+      />
+      <Drawer.Screen 
+        name="CONTACT" 
+        component={Contacts} 
+        options={{ 
+          drawerIcon: ({ focused, size }) => (
+            <Image
+              source={require('../Assets/Contact.png')} 
+              style={[styles.icon, { width: size * 1.5, height: size * 1.5 }] }
+            />
+          ),
+        }} 
+      />
+      <Drawer.Screen 
+        name="CHOOSE APP ICON" 
+        component={ChooseLogo} 
+        options={{ 
+          drawerIcon: ({ focused, size }) => (
+            <Image
+              source={require('../Assets/Choose.png')}
+              style={[styles.icon, { width: size * 1.5, height: size * 1.5 }] }
+            />
+          ),
+        }} 
+      />
+    </Drawer.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  drawer: {
+    backgroundColor: '#000',
+  },
+  logoContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  logo: {
+    width: 120,
+    height: 120, 
+    resizeMode: 'contain',
+  },
+  icon: {
+    resizeMode: 'contain',
+  },
+  footerSpacer: {
+    flex: 1, 
+  },
+  footerContainer: {
+    marginTop: 170,
+    paddingHorizontal: 20,
+    alignItems: 'baseline',
+
+  },
+  footerText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 10,
+    fontWeight: '300',
+  },
+  footerText1: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 10,
+    fontWeight: '300',
+    marginBottom: 15,
+  },
+});
 
 export default DrawerNavigator;
